5540fc640c88831abe4653a0e726a7ca99bf10ca
Sugar Shane <scrisostomo17@gmail.com>
2021-06-26 20:36:07 -0400

    hard coded time color

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,7 +1,10 @@
 //open app and the current day is displayed at the top of the calendar
-var dateElement = $("#currentDay");
+var dateElement = document.getElementById("currentDay");
 var currentDate = moment().format("dddd, MMMM Do, YYYY");  
-dateElement.display = currentDate
+dateElement.innerHTML = currentDate
+
+console.log(currentDate);
+
 
 $(document).ready(function (){
     //save button on click
@@ -19,25 +22,103 @@ $(document).ready(function (){
     function trackTime() {
         //get current time
         var timeNow = moment().hour();
-        var timeBlock = $("#9");
-        console.log(timeBlock);
+        var timeBlock = $("#13").val(13);
+        var realTime = parseInt(timeBlock.val());
+        // $('.time-block').each(function () {
+        //     var timeSection = parseInt($(this).attr("id").split("hour")[1]);
+        
 
         //timeblock is coded for past, present. future
-        if(timeBlock < timeNow){
-            $("#9").removeClass("future");
-            $("#9").removeClass("present");
+        if(realTime < timeNow){
             $("#9").addClass("past");
         }
-        else if (timeBlock === timeNow){
-            $("#9").removeClass("future");
-            $("#9").removeClass("past");
+        else if (realTime === timeNow){
             $("#9").addClass("present");
         }
-        else if (timeBlock > timeNow){
-            $("#9").removeClass("past");
-            $("#9").removeClass("present");
+        else if (realTime > timeNow){
             $("#9").addClass("future");
+        };
+
+        if(realTime < timeNow){
+            $("#10").addClass("past");
         }
+        else if (realTime === timeNow){
+            $("#10").addClass("present");
+        }
+        else if (realTime > timeNow){
+            $("#10").addClass("future");
+        };
+
+        if(realTime < timeNow){
+            $("#11").addClass("past");
+        }
+        else if (realTime === timeNow){
+            $("#11").addClass("present");
+        }
+        else if (realTime > timeNow){
+            $("#11").addClass("future");
+        };
+
+        if(realTime < timeNow){
+            $("#12").addClass("past");
+        }
+        else if (realTime === timeNow){
+            $("#12").addClass("present");
+        }
+        else if (realTime > timeNow){
+            $("#12").addClass("future");
+        };
+
+        if(realTime < timeNow){
+            $("#13").addClass("past");
+        }
+        else if (realTime === timeNow){
+            $("#13").addClass("present");
+        }
+        else if (realTime > timeNow){
+            $("#13").addClass("future");
+        }
+
+        if(realTime < timeNow){
+            $("#14").addClass("past");
+        }
+        else if (realTime === timeNow){
+            $("#14").addClass("present");
+        }
+        else if (realTime > timeNow){
+            $("#14").addClass("future");
+        };
+
+        if(realTime < timeNow){
+            $("#15").addClass("past");
+        }
+        else if (realTime === timeNow){
+            $("#15").addClass("present");
+        }
+        else if (realTime > timeNow){
+            $("#15").addClass("future");
+        };
+
+        if(realTime < timeNow){
+            $("#16").addClass("past");
+        }
+        else if (realTime === timeNow){
+            $("#16").addClass("present");
+        }
+        else if (realTime > timeNow){
+            $("#16").addClass("future");
+        };
+
+        if(realTime < timeNow){
+            $("#17").addClass("past");
+        }
+        else if (realTime === timeNow){
+            $("#17").addClass("present");
+        }
+        else if (realTime > timeNow){
+            $("#17").addClass("future");
+        };
+
     }
     trackTime();
    
@@ -51,6 +132,7 @@ $(document).ready(function (){
     $("#hour-15").val(localStorage.getItem("15"));
     $("#hour-16").val(localStorage.getItem("16"));
     $("#hour-17").val(localStorage.getItem("17"));
+    
 
 });
 
